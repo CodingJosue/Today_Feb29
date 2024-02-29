e69394cbd8637110c56ea92585e65de301e4c404
CodingJosue <holyboom4@gmail.com>
2024-02-29 16:26:32 -0500

    Java uses the concept of a stream to make I/O operation fast sequence of data 0s and 1s raw bytes . The first one we are going to see is stream output that allow us to write on a file:
    it creates a file for you but pay attention to the syntax

--- src/main/java/org/example/FileOutputStreamExample.java
+++ src/main/java/org/example/FileOutputStreamExample.java
@@ -1,4 +1,25 @@
 package org.example;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FileOutputStreamExample {
+    public static void main(String[] args) {
+        // must have a catch close to be able for exceptions
+        try{
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number");
+
+            FileOutputStream fout = new FileOutputStream("C:\\Users\\6257586\\Desktop\\try.txt"); // creates the file for you
+            fout.write(65);
+            fout.close();
+            sc.close();
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+        finally {
+            System.out.println("Success");
+        }
+    }
 }
