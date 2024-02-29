f27b7bcb33d5abe5aad68f2941a228674388948e
CodingJosue <holyboom4@gmail.com>
2024-02-29 17:13:17 -0500

    Files we can read for both file and the nwe write each of them and then write them inside

--- src/main/java/org/example/InputStreamExample.java
+++ src/main/java/org/example/InputStreamExample.java
@@ -1,24 +1,37 @@
 package org.example;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.SequenceInputStream;
 import java.io.FileOutputStream;
 
 public class InputStreamExample {
     public static void main(String[] args) throws Exception {
+        int count = 0;
+    try {
 
-        FileInputStream input1 = new FileInputStream("C:\\Users\\\\6257586\\\\Desktop\\\\try.tx");
-        FileInputStream input2 = new FileInputStream("C:\\Users\\6257586\\Desktop\\sample.tx" );
 
-        FileOutputStream output1  = new FileOutputStream("C:\\Users\\6257586\\Desktop\\kly.txt");
+        FileInputStream input1 = new FileInputStream("C:\\Users\\6257586\\Desktop\\try.tx");
+        FileInputStream input2 = new FileInputStream("C:\\Users\\6257586\\Desktop\\sample.tx");
 
-        SequenceInputStream inst = new SequenceInputStream(input1,input2); // this object holds to object as data
+        FileOutputStream output1 = new FileOutputStream("C:\\Users\\6257586\\Desktop\\kly.txt");
+
+        SequenceInputStream inst = new SequenceInputStream(input1, input2); // this object holds to object as data
 
         int j;
-        while((j = inst.read()) != -1){
-            System.out.println((char) j);
-            output1.write(j);
-        }
 
+        while ((j = input1.read()) != -1) {
+            if (((char) j) ==  'a' || ((char) j) == 'A') {
+                count++;
+                System.out.println(count);
+            }
+
+        }
+        input1.close();
+    }catch (IOException e){
+        System.out.println(e.getMessage());
+    }finally {
+        System.out.println(count);
+    }
     }
 }
