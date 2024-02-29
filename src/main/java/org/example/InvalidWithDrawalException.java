c7dd0512e33fdabdcbcea98a9a2b9b6cf131e41c
CodingJosue <holyboom4@gmail.com>
2024-02-29 15:47:51 -0500

    Our own exception that we extended to tehe class exception with super(msg) to store the message to the upper class

---
+++ src/main/java/org/example/InvalidWithDrawalException.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class InvalidWithDrawalException  extends  Exception{
+
+    public InvalidWithDrawalException(){
+
+    }
+    public InvalidWithDrawalException(String msg){
+        super(msg);
+    }
+}
