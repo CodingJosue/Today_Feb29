ef430c15a26fade2885ae77652e775f511de880a
CodingJosue <holyboom4@gmail.com>
2024-02-29 16:01:04 -0500

    Doing ou exception one in the main which is a class that exeds to Exception but with must hrow it from the method since that exception we created i and we throw it so  it becomes a check exception for java

---
+++ src/main/java/org/example/ExceptionDemo4.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ExceptionDemo4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double initialBalance = sc.nextDouble();
+        try {
+            // nested try and catch
+            try {
+                System.out.println("Enter amount to withdraw: ");
+
+                double withDrawAmount = sc.nextDouble();
+                validateWithDrawalAmount(withDrawAmount);
+
+                double remainingBalance = updateBalance(initialBalance,withDrawAmount);
+
+            }catch (InvalidWithDrawalException e){
+                System.out.println(e.getMessage());
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }   finally {
+            System.out.println("finally blocked executed");
+            sc.close();
+        }
+    }
+    private static  void validateWithDrawalAmount(double withDrawalAmount) throws InvalidWithDrawalException{
+        if(withDrawalAmount <= 0){
+            throw new InvalidWithDrawalException("Withdrawal amount must be greater than zero");
+        }
+    }
+    private static double updateBalance(double initialBalance, double withDrawalAmount) throws  InsuficientFundsException {
+        if(withDrawalAmount > initialBalance){
+            throw  new InsuficientFundsException("Insufficient funds for withdrawal");
+        }
+        return initialBalance  - withDrawalAmount;
+    }
+
+    static class InsuficientFundsException extends  Exception{
+        public InsuficientFundsException(String msg){
+            super(msg);
+        }
+    }
+
+}
